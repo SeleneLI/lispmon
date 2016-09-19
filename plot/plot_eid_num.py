76f6f543a74bf990217ca5f903900a0d88934078
SeleneLI <SeleneLI@github.com>
2016-09-19 22:16:07 +0200

    Figures for 19/09/2016

--- plot/plot_eid_num.py
+++ plot/plot_eid_num.py
@@ -28,7 +28,7 @@ print "Length of Y-axis:", len(eid_num_list)
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams.update({'figure.autolayout': True})
 plt.plot(x, eid_num_list)
-plt.xlabel(r"\textrm{2012/01/07--2016/05/15}", font)
+plt.xlabel(r"\textrm{2012/01/07--2016/09/19}", font)
 plt.ylabel(r"\textrm{EID number}", font)
 plt.xlim(min(x),max(x))
 plt.xticks(fontsize=30, fontname="Times New Roman")
